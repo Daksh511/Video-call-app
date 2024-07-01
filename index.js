6c2271edc5c62261b76b030c59b29b537bd6f0a2
Daksh511 <dakshdarji511@gmail.com>
2024-07-02 00:16:01 +0530

    build+origin

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "*",
+		origin: "https://video-call-app-production.up.railway.app/",
 		methods: [ "GET", "POST" ]
 	}
 });
